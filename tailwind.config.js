3c39f4041ac3167d76a03b0362767e8204da4e60
giacomotolari <tolarigiacomo@gmail.com>
2023-08-24 14:05:15 +0200

    add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "big": "24px",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
-// 
+};
